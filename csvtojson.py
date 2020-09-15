c58eaae3173c7dd78ad2b52d7d26be25a3af1db0
Jenny <finej01@gmail.com>
2020-09-14 22:11:48 -0400

    CSV to JSON conversion works now

--- csvtojson.py
+++ csvtojson.py
@@ -1,9 +1,6 @@
 import csv
 import json
 import os
-import pandas as pd
-from itertools import groupby
-from collections import OrderedDict
 
 
 def main():
@@ -29,49 +26,42 @@ def csv_to_json(csvFilePath, jsonFilePath):
 
     # Open dictreader from csv module
     with open(csvFilePath, encoding='utf-8') as csvf:
-        csvReader = csv.DictReader(csvf)
+        csvfields = 'gameId', 'gameDuration', 'level', 'lastRound', 'Ranked', 'ingameDuration', 'combination', 'champion'
+        csvReader = csv.DictReader(csvf, fieldnames=csvfields)
+        # skip the header because it fucks things over
+        next(csvReader, None)
 
-        # columns = gameId,gameDuration,level,lastRound,Ranked,ingameDuration,combination,champion
-        # Convert each row + add to data dict
-        # gameID is primary key
+        a = [
+            'gameId', 'gameDuration', 'level',
+            'lastRound', 'Ranked', 'ingameDuration'
+            ]
+        b = ['combination', 'champion']
 
-        for rows in csvReader:
-            key = rows['gameId']
-            data[key] = rows
+        # for each row we see...
+        #       "gameId": str
+        #       "gameDuration": str
+        #       "level": str
+        #       "lastRound": str
+        #       "Ranked": str
+        #       "ingameDuration": str
+        #       "combination": string dict
+        #       "champion": string dict
+        # take the key which is the row['gameid'] and define as our key
+        # for each str field, use dict comprehension to add to a temp dict
+        # for each str_dict, use eval to convert str> dict and add to temp
+        # add temp dict to full data dict with the key as the key
+        for row in csvReader:
+            key = row['gameId']
+            temp = {field: row[field] for field in a}
+            temp['combination'] = eval(row['combination'])
+            temp['champion'] = eval(row['champion'])
+            data[key] = temp
 
     # use the json.dumps() with json writer to dump data
     with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
         jsonf.write(json.dumps(data, indent=4))
 
     print('CONVERSION COMPLETE')
-
-
-# # the messed up version
-# def csv_to_json(csvFilePath, jsonFilePath):
-#     # make empty dict for json data
-#     data = {}
-
-#     # Open dictreader from csv module
-#     with open(csvFilePath, encoding='utf-8') as csvf:
-#         csvfields = 'gameId', 'gameDuration', 'level', 'lastRound', 'Ranked', 'ingameDuration', 'combination', 'champion'
-#         csvReader = csv.DictReader(csvf, fieldnames=csvfields)
-#         # IDK what i'm doing here
-#         # for row in csvReader:
-#         #     key = row['gameId']
-#         #     if row['combination']:
-#         #         data['combination'] = row
-#         #         print('hi', row['combination'])
-#         #         for combo in row['combination']:
-#         #             print(combo)
-
-#         #     else:
-#         #         data[key] = row
-
-#     # use the json.dumps() with json writer to dump data
-#     with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
-#         jsonf.write(json.dumps(data, indent=4))
-
-#     print('CONVERSION COMPLETE')
 
 
 main()
